75ac0712348dc35bed1de3017fad87d7fc78d51f
DatTran <khiemtran21012000@gmail.com>
2023-06-01 23:03:34 +0700

    add code for life cycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnChanges, OnDestroy, AfterViewInit, DoCheck, AfterContentInit, AfterViewChecked, AfterContentChecked {
+  
+  @Input() name !: String;
+
+  constructor() {
+    console.log("constructor !");
+  }
+  ngDoCheck(): void {
+    
+  }
+
+  ngOnInit(): void {
+    console.log("On init run !");
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("Ng change")
+  }
+  ngOnDestroy(): void {
+    console.log("On destroy run !");
+  }
+  ngAfterViewInit(): void {
+    console.log("On after view init run !");
+  }
+  ngAfterContentInit(): void {
+    console.log("On content innit run !");
+  }
+  ngAfterViewChecked(): void {
+    console.log("On view checked run !");
+  }
+  ngAfterContentChecked(): void {
+    console.log("On content checked run !");
+  }
+
   title = 'angular-learning';
-  name = 'Cong Dat';
   age = 22;
   clickMe() {
     console.log("clicked !!!")
