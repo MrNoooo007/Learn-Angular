1ee353468399ecca5cae00bf07daec292e980027
DatTran <khiemtran21012000@gmail.com>
2023-06-18 22:35:00 +0700

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,44 +5,5 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnChanges, OnDestroy, AfterViewInit, DoCheck, AfterContentInit, AfterViewChecked, AfterContentChecked {
-  
-  @Input() name !: String;
-
-  constructor() {
-    console.log("constructor !");
-  }
-  ngDoCheck(): void {
-    
-  }
-
-  ngOnInit(): void {
-    console.log("On init run !");
-  }
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log("Ng change")
-  }
-  ngOnDestroy(): void {
-    console.log("On destroy run !");
-  }
-  ngAfterViewInit(): void {
-    console.log("On after view init run !");
-  }
-  ngAfterContentInit(): void {
-    console.log("On content innit run !");
-  }
-  ngAfterViewChecked(): void {
-    console.log("On view checked run !");
-  }
-  ngAfterContentChecked(): void {
-    console.log("On content checked run !");
-  }
-
-  title = 'angular-learning';
-  age = 22;
-  clickMe() {
-    console.log("clicked !!!")
-  }
-
-  currentDate = new Date;
+export class AppComponent {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SignInComponent } from './sign-in/sign-in.component';
+import {MatInputModule} from "@angular/material/input";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NoopAnimationsModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
