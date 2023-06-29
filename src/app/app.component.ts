c6812ec2d007ad60adfab7e9ce05642b74932e57
DatTran <khiemtran21012000@gmail.com>
2023-06-29 17:44:02 +0700

    add code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {map} from "rxjs/operators";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  loadedPosts: any[]  = [];
-  private url = 'https://example-jsi-1-default-rtdb.asia-southeast1.firebasedatabase.app/post.json';
+export class AppComponent {
+  loadedFeature = 'recipe';
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {}
-
-  onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    this.http
-      .post(
-        this.url,
-        postData
-      )
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
-  }
-
-  onFetchPosts() {
-    // Send Http request
-    this.http.get(this.url, {
-      headers: new HttpHeaders({ 'Test-Header': 'Cong dat' })
-    }).pipe(
-      map(response => {
-        const postArrays = [];
-        for(const key in response) {
-          if(response.hasOwnProperty(key)) {
-            // @ts-ignore
-            postArrays.push({...response[key], id: key})
-          }
-        }
-        return postArrays;
-      })
-    ).subscribe(data => {
-      // @ts-ignore
-      this.loadedPosts = data;
-    })
-  }
-
-  onClearPosts() {
-    // Send Http request
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
   }
 }
